e170d71baaf05e49bf9cdbfb9f905879a5167f0e
CaioBalc <balczarekcaio@gmail.com>
2024-03-08 08:15:42 -0300

    Atualizado

---
+++ views/products.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produtos</title>
+</head>
+<body>
+    <h1>Produtos</h1>
+    <div>
+        <a href="">Deslogar</a>
+        <a href="">Carrinho</a>
+    </div>
+</body>
+</html>
