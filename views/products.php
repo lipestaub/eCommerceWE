c271ad1b74fa1bd788ea74830a9a41d6c005952b
CaioBalc <balczarekcaio@gmail.com>
2024-03-12 09:21:00 -0300

    CSS

--- views/products.php
+++ views/products.php
@@ -3,34 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
-
     <title>Produtos</title>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="application/javascript" src="../public/javascript/script.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-KrIt9QyEXCP7CQ2VqU3VjbqVd3BBj3i1pVIei7EX27E34QHmp4IaVxZpdHDf9IvzTK7BA7y1RrDU+OaXYbWv9g==" crossorigin="anonymous" />
     <link rel="stylesheet" href="../public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="../public/fontawesome-free-6.5.1-web/css/all.min.css">
 </head>
 <body>
     <header>
         <img class="square" src="/../public/images/Blue Square.png" alt="blue square" width ="150%" height = "160px">
         <span class="centralize title">Produtos</span>
         <div id="header">
-            <a class="headerLeft" href="/sign-out">< Sair</a>
-            
-            <a class="headerRight" href="/shopping-cart">Carrinho ></a>
+            <a class="headerLeft" href="/sign-out"> <i class="fa-solid fa-right-from-bracket"></i> Sair</a>
+            <a class="headerRight" href="/shopping-cart">Carrinho <i class="fa-solid fa-cart-shopping"></i></a>
         </div>
     </header>
     <main>
-        <?php foreach($products as $productId => $product) { ?>
-            <div class="container">
-                <img src="<?php echo $product['image']; ?>" alt="imagem do produto" height="100" width="100">
-                <span class="info"><?php echo $product['description']; ?></span>
-                <span class="info">R$ <?php echo number_format($product['price'], 2, ',', '.'); ?></span>
-                <button class="buy" onclick="addProduct('<?php echo $productId; ?>');">Comprar</button>
-            </div>
-        <?php } ?>
+        <div class="container">
+            <?php foreach($products as $productId => $product) { ?>
+                <div class="class">
+                    <img class="border" src="<?php echo $product['image']; ?>" alt="imagem do produto" height="200" width="200">
+                    <div class="info"><?php echo $product['description']; ?></div>
+                    <div class="info">R$ <?php echo number_format($product['price'], 2, ',', '.'); ?></div>
+                    <button class="buy" onclick="addProduct('<?php echo $productId; ?>');">Comprar</button>
+                </div>
+            <?php } ?>
+        </div>
     </main>
     <div>
         
