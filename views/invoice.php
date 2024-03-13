92eef586718be5bacbb2f3ba797c2f976ea975d1
CaioBalc <balczarekcaio@gmail.com>
2024-03-13 11:38:51 -0300

    CSS

--- views/invoice.php
+++ views/invoice.php
@@ -4,18 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nota Fiscal</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-KrIt9QyEXCP7CQ2VqU3VjbqVd3BBj3i1pVIei7EX27E34QHmp4IaVxZpdHDf9IvzTK7BA7y1RrDU+OaXYbWv9g==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="../public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="../public/fontawesome-free-6.5.1-web/css/all.min.css">
+
 </head>
 <body>
     <header>
-        <h1>Nota Fiscal</h1>
+        <img src="../public/images/Blue Square.png" alt="blue square" width="100%" height = "160px">
+        <span class="centralize title">Nota Fiscal </span>
+        <div id="header">
+            <a class="headerLeft" href="/products"><i class="fa-solid fa-house" style="color: #ffffff;"></i> Produtos</a>
+        </div>
     </header>
     <main>
         <div>
-            <span>Nome: <?= $user->getName(); ?></span>
-            <span>CPF: <?= $user->getCpf(); ?></span>
+            <span class="descriptionCart">Nome: <span class="total"><?= $user->getName(); ?></span></span>
+            <span class="descriptionCart">CPF: <span class="total"><?= $user->getCpf(); ?></span></span>
         </div>
-        <table>
-            <tr>
+        <table class="centralize">
+            <tr class="descriptionCart">
                 <th>Descrição</th>
                 <th>Preço</th>
                 <th>Quantidade</th>
@@ -23,7 +34,7 @@
             </tr>
             <?php
                 foreach ($products as $productId => $product) { ?>
-                    <tr>
+                    <tr class="info">
                         <td>
                             <span><?= $product->getDescription(); ?></span>
                         </td>
@@ -40,7 +51,7 @@
             <?php } ?>
         </table>
         <br>
-        <span>Valor Total: R$ <?= number_format($total, 2, ',', '.'); ?></span>
+        <span class="descriptionCart">Valor Total: <span class="total">R$ <?= number_format($total, 2, ',', '.'); ?></span></span>
     </main>
 </body>
 </html>
